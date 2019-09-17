8a576df984fa9472db006c101c2aa1d2cab12042
John Coyle <dx9err@gmail.com>
2019-09-16 21:11:21 -0700

    Fix MacOSX 10.15 CC_MD5 deprecation errors
    Use CC_SHA1 instead.
    
    Signed-off-by: John Coyle <dx9err@gmail.com>

--- src/smbiostbl.c
+++ src/smbiostbl.c
@@ -598,8 +598,8 @@ smbios_type1_initializer(struct smbios_structure *template_entry,
 
 		uuid_enc_le(&type1->uuid, &uuid);
 	} else {
-		CC_MD5_CTX	mdctx;
-		u_char		digest[16];
+		CC_SHA1_CTX	mdctx;
+		u_char		digest[CC_SHA1_DIGEST_LENGTH];
 		char		hostname[MAXHOSTNAMELEN];
 
 		/*
@@ -610,10 +610,10 @@ smbios_type1_initializer(struct smbios_structure *template_entry,
 		if (gethostname(hostname, sizeof(hostname)))
 			return (-1);
 
-		CC_MD5_Init(&mdctx);
-		CC_MD5_Update(&mdctx, vmname, ((unsigned) strlen(vmname)));
-		CC_MD5_Update(&mdctx, hostname, ((unsigned) sizeof(hostname)));
-		CC_MD5_Final(digest, &mdctx);
+		CC_SHA1_Init(&mdctx);
+		CC_SHA1_Update(&mdctx, vmname, ((unsigned) strlen(vmname)));
+		CC_SHA1_Update(&mdctx, hostname, ((unsigned) sizeof(hostname)));
+		CC_SHA1_Final(digest, &mdctx);
 
 		/*
 		 * Set the variant and version number.
